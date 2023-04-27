85a3d06407b47b8f3298c1bbdb06b011129a64ca
Rish <55277468+rishrs7@users.noreply.github.com>
2023-04-27 04:04:19 +0100

    Deployment-test-3

--- flavorful_main/settings.py
+++ flavorful_main/settings.py
@@ -151,14 +151,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    'flavorful_main/static'
+    os.path.join(BASE_DIR,'flavorful_main/static')
 ]
 
 # Media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
