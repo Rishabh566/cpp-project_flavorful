95d3cdb2ffeb74caf59afa7d53f3e99080432631
Rish <55277468+rishrs7@users.noreply.github.com>
2023-04-28 01:32:46 +0100

    not happening

--- flavorful_main/settings.py
+++ flavorful_main/settings.py
@@ -154,15 +154,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'flavorful_main/static'),
+# ]
+
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR/ 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'flavorful_main/static'),
 ]
-
 # Media files configuration
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR/ 'media'
 MEDIA_URL = '/media/'
 
 
